d8d11942f4e98626abc67c23bdff7b7d0896aae2
raj <radjasegar.raj@gmail.com>
2017-01-08 00:33:09 +0100

    Premier JET : DAO Customer : id, First Name, Last Name

--- src/main/java/isep/web/sakila/dao/business/Business.java
+++ src/main/java/isep/web/sakila/dao/business/Business.java
@@ -9,8 +9,10 @@ import com.google.common.collect.Lists;
 
 import isep.web.sakila.dao.repositories.ActorRepository;
 import isep.web.sakila.dao.repositories.FilmRepository;
+import isep.web.sakila.dao.repositories.CustomerRepository;
 import isep.web.sakila.jpa.entities.Actor;
 import isep.web.sakila.jpa.entities.Film;
+import isep.web.sakila.jpa.entities.Customer;
 
 @Service("business")
 public class Business implements IBusiness
@@ -18,6 +20,7 @@ public class Business implements IBusiness
 	@Autowired
 	private ActorRepository actorRepository;
 	private FilmRepository filmRepository;
+	private CustomerRepository customerRepository;
 
 	@Override
 	public List<Actor> getAllActors()
@@ -39,4 +42,15 @@ public class Business implements IBusiness
 	public Film getFilmByID(int filmId) {
 		return filmRepository.findOne(filmId);
 	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		return Lists.newArrayList(customerRepository.findAll());
+		}
+
+	@Override
+	public Customer getCustomerByID(int customerId) {
+		return customerRepository.findOne(customerId);
+	}
+
 }
--- src/main/java/isep/web/sakila/dao/business/IBusiness.java
+++ src/main/java/isep/web/sakila/dao/business/IBusiness.java
@@ -4,13 +4,27 @@ import java.util.List;
 
 import isep.web.sakila.jpa.entities.Actor;
 import isep.web.sakila.jpa.entities.Film;
+import isep.web.sakila.jpa.entities.Customer;
 
 public interface IBusiness
 {
+	/*
+	 * For Actors
+	 */
 	public List<Actor> getAllActors();
-
 	public Actor getActorByID(int actorId);
 
+	/*
+	 * For Films
+	 */
 	public List<Film> getAllFilms();
 	public Film getFilmByID(int filmId);
+	
+	/*
+	 * For Customers
+	 */
+	public List<Customer> getAllCustomers();
+	public Customer getCustomerByID(int customerId);
+
+
 }
