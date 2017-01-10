83dc7601fafd530231f8b436e9786401992bb923
raj <radjasegar.raj@gmail.com>
2017-01-10 17:05:08 +0100

    Supression des fichiers avec espaces

--- src/main/java/isep/web/sakila/dao/repositories/CityRepository .java
+++
@@ -1,9 +0,0 @@
-package isep.web.sakila.dao.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import isep.web.sakila.jpa.entities.City;
-
-public interface CityRepository extends CrudRepository<City, Integer>
-{
-}
--- src/main/java/isep/web/sakila/dao/repositories/LanguageRepository .java
+++
@@ -1,9 +0,0 @@
-package isep.web.sakila.dao.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import isep.web.sakila.jpa.entities.Language;
-
-public interface LanguageRepository extends CrudRepository<Language, Integer>
-{
-}
